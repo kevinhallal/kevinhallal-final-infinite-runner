ecd6fef7ea204aab8415916e40f790d5b382fc7e
kevinhallal <kevin.hallall@gmail.com>
2026-05-14 21:43:32 +0300

    Add powerup inventory banner HUD

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 
 public class HUDManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text speedBoostAmountText;
+[SerializeField] private TMP_Text magnetAmountText;
+[SerializeField] private TMP_Text invincibilityAmountText;
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private TMP_Text highScoreText;
     [SerializeField] private TMP_Text coinText;
 
     void Update()
     {
+        speedBoostAmountText.text = "x" + GameManager.Instance.SpeedBoostCount;
+magnetAmountText.text = "x" + GameManager.Instance.MagnetCount;
+invincibilityAmountText.text = "x" + GameManager.Instance.InvincibilityCount;
         if (GameManager.Instance == null || scoreText == null) return;
 
         int score = Mathf.FloorToInt(GameManager.Instance.Distance);
@@ -17,5 +23,6 @@ public class HUDManager : MonoBehaviour
         if (coinText != null)
    coinText.text = "Coins: " + GameManager.Instance.RunCoins 
               + " | Total: " + GameManager.Instance.TotalCoins;
+              
     }
 }
