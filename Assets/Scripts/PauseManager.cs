7957b89104f49a3295aca447e8db4f6fceac5bf8
kevinhallal <kevin.hallall@gmail.com>
2026-05-14 11:48:12 +0300

    Disable pause controls after game over

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,17 +1,26 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PauseManager : MonoBehaviour
 {
     [SerializeField] private GameObject pauseUI;
+    [SerializeField] private GameObject pauseButton;
 
     private bool isPaused;
 
     void Update()
     {
         if (GameManager.Instance != null && GameManager.Instance.IsGameOver)
+        {
+            if (pauseUI != null)
+                pauseUI.SetActive(false);
+
+            if (pauseButton != null)
+                pauseButton.SetActive(false);
+
+            Time.timeScale = 1f;
             return;
+        }
 
         if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
         {
@@ -21,6 +30,9 @@ public class PauseManager : MonoBehaviour
 
     public void TogglePause()
     {
+        if (GameManager.Instance != null && GameManager.Instance.IsGameOver)
+            return;
+
         isPaused = !isPaused;
 
         Time.timeScale = isPaused ? 0f : 1f;
