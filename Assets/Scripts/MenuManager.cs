4ce094c3a14cf9e8943739e45d08a6a644d043a2
kevinhallal <kevin.hallall@gmail.com>
2026-05-14 11:42:32 +0300

    Add main menu scene and scene management

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Quit Game");
+    }
+}
